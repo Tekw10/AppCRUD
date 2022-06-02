fb4407498091b972b1ceb196f0616c37572f0155
Tekw10 <100322401+Tekw10@users.noreply.github.com>
2022-06-01 21:36:28 -0400

    com

--- appcrud/src/app/servicos/cliente.service.ts
+++ appcrud/src/app/servicos/cliente.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 export interface Cliente{
   id: number;
